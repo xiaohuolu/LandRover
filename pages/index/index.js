992f8b1428b524d488393cac4de8e523e55e4f34
cxn <593900745@qq.com>
2018-05-15 16:49:03 +0800

    路虎

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+ import testDrive from "../../modules/test-drive.js"
+const app=getApp();
+
+Page({
+  data:{
+    slides:null,   
+    entities:null
+  },
+testDrive,
+  readMore(event){
+    wx.navigateTo({
+      url:`/pages/vehicles/show?id=${event.target.dataset.id}`
+    })
+  },
+
+  onLoad(){
+    this.setData({
+      slides:app.globalData.slides,
+      entities:app.globalData.vehicles
+    })
+  }
+})
